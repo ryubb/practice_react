76586c669fb637199afc3ff9962a1182a03db9bb
ryubb <ryubb1108@gmail.com>
2019-07-09 13:52:16 +0900

    feat:react-router-domを用いて、ルーティングの設定

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React from "react";
 import styled from "react-emotion";
+import { Link, Route, Switch } from "react-router-dom";
 
-// import HooksComponent from "./components/HooksComponent";
 // import LabbaseSTGAxios from "./components/LabbaseSTGAxios";
 import RailsAPI from "./components/RailsAPI";
 import BubbleEvent from "./components/BubbleEvent";
+import HooksComponent from "./components/HooksComponent";
+import Children from "./components/Children";
 
 const Body = styled("div")`
   width: 1024px;
   margin: 0 auto;
 `;
 
+const Header = styled("div")`
+  width: 100%;
+  height: 50px;
+  background-color: f5f5f5;
+`;
+
+const CustomLink = styled(Link)`
+  margin-right: 10px;
+`;
+
+const links = [
+  { name: "Rails API", link_to: "/railsAPI" },
+  { name: "Event Bubble", link_to: "/eventBubble" },
+  { name: "Children", link_to: "/children" },
+  { name: "Hooks", link_to: "/hooks" }
+];
+
 const App = () => {
   return (
     <Body>
-      <RailsAPI />
-      <BubbleEvent />
+      <Header>
+        {links.map(link => {
+          return (
+            <CustomLink key={link.link_to} to={link.link_to}>
+              {link.name}
+            </CustomLink>
+          );
+        })}
+      </Header>
+      <Switch>
+        <Route path="/railsAPI" component={RailsAPI} />
+        <Route path="/eventBubble" component={BubbleEvent} />
+        <Route path="/children" component={Children} />
+        <Route path="/hooks" component={HooksComponent} />
+      </Switch>
     </Body>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
@@ -13,7 +14,9 @@ const store = createStore(loginReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
